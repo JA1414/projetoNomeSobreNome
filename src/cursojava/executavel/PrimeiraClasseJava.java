d6c475a99e8b307d9f485242bfe6be7cac6a47f8
Jean Heberth <jeanheberthh@gmail.com>
2022-12-19 20:29:25 -0300

    init

---
+++ src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,12 @@
+package cursojava.executavel;
+
+public class PrimeiraClasseJava {
+ 
+	/*Main é um metodo auto executavel em Java*/
+	public static void main(String[] args) {
+		
+		/*Escreve texto no console*/
+		System.out.println("Meu primeiro progama em Java - Qual foi o seu?");
+		System.out.println("Meu primeiro progama em Java - foi quando eu tinha 13 anos");
+	}
+}
